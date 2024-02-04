4331e44aa5717294a4179769d8106a760008e298
Manya <134845162+manyaa1@users.noreply.github.com>
2024-02-04 21:13:44 +0530

    Create ternary.cpp

---
+++ ternary.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+
+    cout<<"Enter your marks";
+    cin>> marks;
+
+    marks>=60 ? cout<<"Pass" : cout<<"Fail";
+
+return 0;
+
+}
