023354cc452084e9d626a6319c778211dd107ebd
Manya <134845162+manyaa1@users.noreply.github.com>
2024-02-13 22:17:16 +0530

    Create pattern22.cpp

---
+++ pattern22.cpp
@@ -0,0 +1,21 @@
+//1
+//11
+//111
+//1111
+//11111
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<1;
+
+        }
+        cout<<endl;
+
+
+    }
+	return 0;
+}
