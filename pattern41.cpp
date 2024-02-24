c53f57b3a5f75e48de337385879f096fad33ce7d
Manya <134845162+manyaa1@users.noreply.github.com>
2024-02-24 21:22:49 +0530

    Hollow diamond
    * * * * * * * * *
    * * * *   * * * *
    * * *       * * *
    * *           * *
    *               *
    * *           * *
    * * *       * * *
    * * * *   * * * *
    * * * * * * * * *

---
+++ pattern41.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for (int i=1;i<=n/2+1;i++){
+    
+            for(int j=1;j<=(n/2-i+2);j++){
+                cout<<"*"<<" ";
+            }
+           
+            for(int j=1;j<(2*i-2);j++){
+                cout<<"  ";
+
+            
+            }
+            for(int j=1;j<=(n/2-i+2);j++){
+                if(i==1 && j==(n/2-i+2))
+				break;
+				cout <<"*"<<" ";
+					
+			 }
+             cout<<endl;
+            
+        }
+    for(int i=n/2;i>=1;i--){
+			
+            for(int j=(n/2-i+2);j>=1;j--){
+             
+                cout<<"*"<<" ";
+
+            }//2*(i+1)-n
+            
+			for(int j=1;j<=2*(i-1)-1;j++){
+                cout<<"  ";
+            }
+            for(int j=(n/2-i+2);j>=1;j--){
+                if(i==1 && j==1)break;
+             
+                cout<<"*"<<" ";
+
+            }
+   // for(int j=1;j<=(n/2-i+2);j++)
+     ///               if(i==1 && j>(n/2-i+2))
+        //              break;
+                     
+		//			  {
+      //               cout<<"*"<<" ";
+          //            }
+				  cout<<endl;
+                    
+         
+            }
+            return 0;
+}
