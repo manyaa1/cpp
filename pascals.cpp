1dd3fd91a1567180828910e65eedcf2508b7f81e
Manya <134845162+manyaa1@users.noreply.github.com>
2024-02-27 20:48:34 +0530

    Create pascals.cpp

---
+++ pascals.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    
+    cin >> n;
+    
+ 
+    for (int i = 0; i < n; i++)
+    {
+        int val = 1;
+        for (int j = 1; j < (n - i); j++)
+        {
+            cout << "   ";
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            cout << "      " << val;
+            val = val * (i - k) / (k + 1);
+        }
+        cout << endl << endl;
+    }
+    cout << endl;
+    return 0;
+}
